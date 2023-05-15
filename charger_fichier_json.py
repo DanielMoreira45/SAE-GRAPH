7d7e69350a8a52ce9a842e860c972dfa6adb2ab8
daniel_moreira <daniel.moreira.41160@gmail.com>
2023-05-15 12:08:02 +0200

    finalisation de la l'imple du fichier json

--- charger_fichier_json.py
+++ charger_fichier_json.py
@@ -15,23 +15,55 @@ def convert_txt_to_dict(nom_fichier):
     Returns:
         list: la liste des résultats contenus dans le fichier
     """
-    data = dict()
-    try:
-        liste_resultat = []
-        fic = open(nom_fichier, "r", encoding="utf8")
-        titre = ""
-        for ligne in fic:
-            liste_petit_champs = []
-            champs = ligne.split(":")
-            for liste in champs:
-                petit_champs = liste.split(",")
-                print(petit_champs)
-                liste_petit_champs.append(petit_champs)
-            print(liste_petit_champs)
-            #dict[].append((int(champs[0]), champs[1], int(champs[2]), int(champs[3]), int(champs[4])))
-        return liste_resultat
-    except IOError:
-        print("Le nom/chemin du fichier indiqué n'existe pas !")
-    pass
 
-convert_txt_to_dict("little_data.txt")
+    with open(nom_fichier) as fichier_open:
+        lignes = fichier_open.readlines()
+        dico = dict()
+        for json_data in lignes:
+            Data =json.loads(json_data)
+            titre = Data["title"]
+            cast = Data["cast"]
+            try :
+                directeur = Data["directors"]
+            except:
+                directeur = list()
+            try :
+                producteur = Data["producers"]
+            except:
+                producteur = list()
+            try :
+                companies = Data["companies"]
+            except:
+                companies = list()
+            try:
+                annee_sorti = Data["year"]
+            except:
+                annee_sorti = list()
+            Vtitre = titre.replace("[[", "").replace("]]", "")
+            Vcast = list()
+            if len(cast) > 0:
+                for personne in cast:
+                    Vcast.append(personne.replace("[[", "").replace("]]", ""))
+            
+            Vdirecteur = list()
+            if len(directeur) > 0:
+                for director in directeur:
+                    Vdirecteur.append(director.replace("[[", "").replace("]]", ""))
+            Vproducteur = list()
+            if len(producteur) > 0:
+                for prod in producteur:
+                    Vproducteur.append(prod.replace("[[", "").replace("]]", ""))
+
+            Vcompanies = list()
+            if len(companies) > 0:
+                for comp in companies:
+                    Vcompanies.append(comp.replace("[[", "").replace("]]", ""))
+            dico[Vtitre] = dict()
+            dico[Vtitre]["cast"] = Vcast
+            dico[Vtitre]["director"] = Vdirecteur
+            dico[Vtitre]["producers"] = Vproducteur
+            dico[Vtitre]["companies"] = Vcompanies
+            dico[Vtitre]["year"] = annee_sorti  
+            
+# convert_txt_to_dict("./little_data.txt")
+convert_txt_to_dict("./data.txt")
